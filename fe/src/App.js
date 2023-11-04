b5ee5a0d792f6f0f199e7dab71bb8df9fb4a5e9b
canhtuan <tuan.tc@orai.io>
2023-11-04 15:43:53 +0700

    132

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,30 +1,88 @@
 import logo from "./logo.svg";
+import { Line } from "react-chartjs-2";
+import Chart from "chart.js/auto";
 import "./App.css";
-import {
-  XYPlot,
-  XAxis,
-  YAxis,
-  HorizontalGridLines,
-  LineSeries,
-} from "react-vis";
+import faker from "faker";
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
+
+const options = {
+  responsive: true,
+  interaction: {
+    mode: "index",
+    intersect: false,
+  },
+  stacked: false,
+  plugins: {
+    title: {
+      display: true,
+      text: "Chart.js Line Chart - Multi Axis",
+    },
+  },
+  scales: {
+    y: {
+      type: "linear",
+      display: true,
+      position: "left",
+
+      y1: {
+        type: "linear",
+        display: true,
+        position: "right",
+        grid: {
+          drawOnChartArea: false,
+        },
+      },
+    },
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
 
 function App() {
+  const [data, setData] = useState();
+
+  const getData = async () => {
+    const dataChart = await axios.request({
+      method: "get",
+      maxBodyLength: Infinity,
+      url: "http://127.0.0.1:9000/data",
+      headers: {},
+    });
+
+    if (dataChart.status !== 500) {
+      setData({
+        labels: dataChart.data.Date.map((value) => new Date(value)),
+        datasets: [
+          {
+            label: "Dataset 2",
+            data: dataChart.data.Price,
+            borderColor: "rgb(53, 162, 235)",
+            backgroundColor: "rgba(53, 162, 235, 0.5)",
+            yAxisID: "y1",
+          },
+        ],
+      });
+    }
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  // const interval = setInterval(() => {
+  //   console.log("Fetching data...");
+  //   getData();
+  // }, 5000);
+
   return (
-    <div className="App">
-      <div>canhtuan</div>
-      <XYPlot width={300} height={300}>
-        <HorizontalGridLines />
-        <LineSeries
-          color="red"
-          data={[
-            { x: 1, y: 10 },
-            { x: 2, y: 5 },
-            { x: 3, y: 15 },
-          ]}
-        />
-        <XAxis title="X" />
-        <YAxis />
-      </XYPlot>
+    <div>
+      <div>ca</div>
+      {data && (
+        <div>
+          <Line options={options} data={data} />
+        </div>
+      )}
     </div>
   );
 }
